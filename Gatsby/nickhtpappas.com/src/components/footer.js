79c80f413c6ecf7df7d695b906e155f06742f8e0
Nick <nickhtpappas@gmail.com>
2018-09-16 16:16:08 -0700

    Broke something changing the footer. Commit before I try to undo it.

--- Gatsby/nickhtpappas.com/src/components/footer.js
+++ Gatsby/nickhtpappas.com/src/components/footer.js
@@ -5,7 +5,53 @@ import '../style/footer.css'
 const Footer = () => {
   return (
     <footer className="footer center">
-      <h6>Copyright &copy; {new Date().getFullYear()} Nicholas Pappas</h6>
+      <div className="footer-cell-one ">
+        <h6>Social</h6>
+        <div className="footer-links">
+          <a href="#">
+            <i className="fa fa-linkedin" />
+          </a>
+          <a href="#">
+            <i className="fa fa-github" />
+          </a>
+          <a href="#">
+            <i className="fa fa-codepen" />
+          </a>
+          <a href="#">
+            <i className="fa fa-twitter" />
+          </a>
+        </div>
+      </div>
+      <div className="footer-cell-two ">
+        <div className="copyright-cell ">
+          <h6>&copy; {new Date().getFullYear()} </h6>
+        </div>
+        <div className="copyright-cell-name">
+          <div className="copyright-cell ">
+            <h5>Nicholas</h5>
+          </div>
+          <div className="copyright-cell ">
+            <h5>Pappas</h5>
+          </div>
+        </div>
+      </div>
+      <div className="footer-cell-three ">
+        <h6>Site</h6>
+        <div className="footer-links">
+          <a href="#">
+            <h5>Home</h5>
+          </a>
+          <a href="#">
+            <h5>About</h5>
+          </a>
+          <a href="#">
+            <h5>Contact</h5>
+          </a>
+          <a href="#">
+            <h5>Portfolio</h5>
+          </a>
+        </div>
+      </div>
     </footer>
   )
 }
